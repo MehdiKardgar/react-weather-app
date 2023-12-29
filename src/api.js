4557bebc51f01569a53b4cc85803c76dbc4ec592
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2023-12-29 18:55:38 +0330

    search city
    search city

---
+++ src/api.js
@@ -0,0 +1,11 @@
+// https://rapidapi.com/wirefreethought/api/geodb-cities/
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b7610c8cc0mshbfc7a7e4418b9ecp1862a0jsn538ee6dd4521", // "key" is unice that it comes from your account
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_APL_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
