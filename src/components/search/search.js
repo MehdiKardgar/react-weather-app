b64cf5ea2934d6538b2ebc6d33f76e2fed5efdf2
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2024-01-03 22:01:10 +0330

    get dynamic data

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_APL_URL, geoApiOptions } from "../../api";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 function Search({ onSearchChange }) {
   const [search, setSearch] = useState(null);
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_APL_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
